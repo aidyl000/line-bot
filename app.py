f44e2e0b25b3ea434b546291b75e622f147978ab
Lydia <lyseop0105@gmail.com>
2020-08-11 20:55:27 +0800

    update

--- app.py
+++ app.py
@@ -40,15 +40,15 @@ def handle_message(event):
     msg = event.message.text
     r = "I don't understand"
     if 'sticker' in msg:
-    sticker_message = StickerSendMessage(
-        package_id='1',
-        sticker_id='1'
-    )  
+        sticker_message = StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        )  
 
-    line_bot_api.reply_message(
-    event.reply_token,
-    sticker_message)
-    return
+        line_bot_api.reply_message(
+            event.reply_token,
+            sticker_message)
+        return
     
     if msg in ['hi', 'Hi']:
         r = "hello"
